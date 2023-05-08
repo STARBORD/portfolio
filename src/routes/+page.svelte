5e864931866ab20952d90984ce3d125ca10baec7
Woohyun Seong <uve0508@naver.com>
2023-05-08 21:01:53 +0900

    test message and some tries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    .stress4{
+        font-size: 14pt;
+        color: goldenrod;
+        text-decoration: underline;
+        
+    }
+</style>
+
+<h1>hi</h1>
+<h4>저는 <span class="stress4">창녕 성씨(昌寧 成氏) 상곡공파(桑谷公派) 29대손 성우현(成優賢)</span> 입니다.</h4>
